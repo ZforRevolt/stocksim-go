dc29ea54f0f6957dfb06081a8af5791372881423
Adam Woodbeck <adam@woodbeck.net>
2015-09-15 12:05:14 -0400

    Added an index.html template.

--- stocksim.go
+++ stocksim.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	p := "assets/index.html"
+	if _, err := os.Stat(p); err == nil {
+		http.ServeFile(w, r, p)
+		return
+	}
+	http.NotFound(w, r)
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", indexHandler)
+	log.Fatal(http.ListenAndServe("localhost:8800", router))
+}
